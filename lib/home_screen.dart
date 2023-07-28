15b841cfd3ee6e0dfe0ca3521aa57a4e3cd127d1
Ranim-Rabah <79060004+Farah-Torkhani@users.noreply.github.com>
2023-07-28 12:11:05 +0100

    edited

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,10 +1,136 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  List foods = [
+    "Burger",
+    "Pizza",
+    "Snacks",
+    "Water",
+  ];
 
+  List foods2 = ["chicken Burger", "cheese Pizza"];
+  List<Color> bgColors = [
+    Color(0xFFFBDCDA),
+    Color(0xFFD4EEF3),
+    Color(0xFFFAE6D5),
+    Color(0xffEFCFE7),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Material(
+      color: Colors.white,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 50),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        "deliver to",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.black54,
+                        ),
+                      ),
+                      SizedBox(height: 5),
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.location_on,
+                            color: Color(0xFFFF2F08),
+                          ),
+                          const Text(
+                            "New York, USA",
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                          const Icon(
+                            Icons.arrow_drop_down,
+                            color: Color(0xFFFF2F08),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                  Stack(
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 50,
+                        margin: EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          image: DecorationImage(
+                            image: AssetImage("images/profile.jpg"),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 40,
+                        child: Container(
+                          margin: EdgeInsets.all(5),
+                          padding: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.white, width: 3),
+                            color: Color(0xFFFF2F08),
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.4,
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              decoration: BoxDecoration(
+                color: Color(0xFFF3F3F3),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Center(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Search your food here..",
+                    border: InputBorder.none,
+                    prefixIcon: Icon(Icons.search),
+                  ),
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: Container(
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: Color(0xFFFF2F08),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+               SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Image.asset("images/banner.jpg"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
